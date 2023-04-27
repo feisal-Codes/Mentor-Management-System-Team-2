7cbb9f706ab10077839bb0a6ec1f146b0e7653e3
feisal-Codes <feisalmib@gmail.com>
2023-04-27 21:19:42 +0300

    removed logs

--- backend/app/Controllers/Http/ProfilesController.ts
+++ backend/app/Controllers/Http/ProfilesController.ts
@@ -21,7 +21,6 @@ export default class ProfilesController {
 
   async update({ auth, request, response }: HttpContextContract) {
     try {
-      console.log(request)
       if(auth.user?.id){
         const userId = auth.user?.id
       const profile = await User.findOrFail(userId)
