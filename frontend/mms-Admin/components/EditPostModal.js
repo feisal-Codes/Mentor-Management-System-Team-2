b82e3cfe3acf1ac44bc7637e572928c410bf6da2
feisal-Codes <feisalmib@gmail.com>
2023-05-03 23:06:43 +0300

    edit post fix

--- frontend/mms-Admin/components/EditPostModal.js
+++ frontend/mms-Admin/components/EditPostModal.js
@@ -55,7 +55,6 @@ export const EditPostModal = ({
   const handleSubmit = async (event) => {
     //call api here
     event.preventDefault();
-    console.log(postData);
     try {
       if (!postData.title || !postData.description) {
         setConfirmLoading(false);
